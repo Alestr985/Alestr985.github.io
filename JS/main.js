7da7ddab926fcb2d159e056f94b5ecfe72a324cb
Alestr <user201503@gmail.com>
2020-08-29 13:31:58 +0800

    планшетная версия и фикс багов

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,15 @@
 let l=0;
 
-document.getElementById("min_menu_title").onclick = function(){
+document.getElementsByClassName("min-menu__title")[0].onclick = function(){
 	if (l%2==0){
-		document.getElementsByClassName("min_menu_item")[0].style.display = "block";
-		document.getElementsByClassName("min_menu_item")[1].style.display = "block";
-		document.getElementsByClassName("min_menu_item")[2].style.display = "block";
+		document.getElementsByClassName("min-menu__item")[0].style.display = "block";
+		document.getElementsByClassName("min-menu__item")[1].style.display = "block";
+		document.getElementsByClassName("min-menu__item")[2].style.display = "block";
 	}
 	if (l%2==1){
-		document.getElementsByClassName("min_menu_item")[0].style.display = "none";
-		document.getElementsByClassName("min_menu_item")[1].style.display = "none";
-		document.getElementsByClassName("min_menu_item")[2].style.display = "none";
+		document.getElementsByClassName("min-menu__item")[0].style.display = "none";
+		document.getElementsByClassName("min-menu__item")[1].style.display = "none";
+		document.getElementsByClassName("min-menu__item")[2].style.display = "none";
 	}
 	l++;
 }
