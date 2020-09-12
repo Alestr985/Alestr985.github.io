2183796b28844650bc1e4710748fbe9faa0baf14
Alestr <user201503@gmail.com>
2020-09-12 22:17:11 +0800

    фикс шрифтов и изменен шаг адаптивности

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,1 @@
-let l=0;
-
-document.getElementsByClassName("min-menu__title")[0].onclick = function(){
-	if (l%2==0){
-		document.getElementsByClassName("min-menu__item")[0].style.display = "block";
-		document.getElementsByClassName("min-menu__item")[1].style.display = "block";
-		document.getElementsByClassName("min-menu__item")[2].style.display = "block";
-	}
-	if (l%2==1){
-		document.getElementsByClassName("min-menu__item")[0].style.display = "none";
-		document.getElementsByClassName("min-menu__item")[1].style.display = "none";
-		document.getElementsByClassName("min-menu__item")[2].style.display = "none";
-	}
-	l++;
-}
+console.log(0.1+0.2)
