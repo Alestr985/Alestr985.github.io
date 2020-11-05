0af337dd681837bf726d00da146bb797e6aef783
Alestr <user201503@gmail.com>
2020-11-05 19:09:18 +0800

    фикс багов

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,10 @@
-console.log(0.1+0.2)
+document.getElementById("calculator").onsubmit = function(event) {
+	event.preventDefault();
+  var eruption = document.getElementsByName("eruption")[0].value;
+  var ec = document.getElementsByName("ec")[0].value / 600;
+  var aec = document.getElementsByName("aec")[0].value / 600;
+  var ac = document.getElementsByName("ac")[0].value;
+  var cycles = document.getElementsByName("cycles")[0].value;
+  var result = (ac/aec*ec*eruption);
+  document.getElementById("result").innerHTML = result+" kg";
+    }
